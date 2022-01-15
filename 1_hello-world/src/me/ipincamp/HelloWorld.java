2099c509adc3b2f4c4fac1807985932d70074575
ipincamp <arif.lngsrn@gmail.com>
2022-01-15 10:27:03 +0700

    Change directory, add hello-world & variables

---
+++ 1_hello-world/src/me/ipincamp/HelloWorld.java
@@ -0,0 +1,10 @@
+package me.ipincamp;
+
+public class HelloWorld {
+    public static void main(String [] args) {
+        /**
+         * Hello World
+         */
+        System.out.println("This is hello world!");
+    }
+}
