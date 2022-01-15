f0e1978db54f0bc12c43be37c5bbdf8b11f1f901
ipincamp <arif.lngsrn@gmail.com>
2022-01-15 08:56:48 +0700

    Arithmetic Operators

---
+++ arithmetic-operators/src/main/java/me/ipincamp/Main.java
@@ -0,0 +1,18 @@
+package me.ipincamp;
+
+public class Main {
+    public static void main(String [] args) {
+        int number = 20;
+        number = number + 30;
+        System.out.println("Sum: " + number);
+
+        /**
+         * + sum
+         * - substraction
+         * * multiplication
+         * / distribution
+         * % modulus
+         */
+        System.out.println(number / 8 + "." + number % 9);
+    }
+}
