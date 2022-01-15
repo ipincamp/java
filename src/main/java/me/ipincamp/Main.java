139b552f26eeb94039fea887f3160d57bd31e381
ipincamp <arif.lngsrn@gmail.com>
2022-01-15 10:08:13 +0700

    Refactoring modules & Changing template workspace

---
+++ src/main/java/me/ipincamp/Main.java
@@ -0,0 +1,16 @@
+package me.ipincamp;
+
+public class Main {
+    public static void main(String [] args) {
+        /**
+         * Basic Java Course
+         *
+         * Java language course on channel Worn Off Keys,
+         * this is a basic course in 2020.
+         * I made this repository as my documentation in the future.
+         *
+         * Source: Alexzander Flores
+         */
+        System.out.println("ipincamp > Don\'t waste your time!");
+    }
+}
