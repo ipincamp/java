9082c14cb7fce5ce7b71c4f76ae36f8fd08a64f3
ipincamp <arif.lngsrn@gmail.com>
2022-01-16 16:25:25 +0700

    Increment and Decrement

---
+++ 5_increment-decrement/src/me/ipincamp/IncrementDecrement.java
@@ -0,0 +1,24 @@
+package me.ipincamp;
+
+public class IncrementDecrement {
+    public static void main(String [] args) {
+        /**
+         * Increase
+         */
+        int number = 50;
+
+        number++;
+        System.out.println(number);
+        /**
+         * Decrease
+         *
+         * name++;
+         *
+         * ++name; for cross output
+         */
+        int myRoles = 50;
+
+        myRoles--;
+        System.out.println(myRoles);
+    }
+}
