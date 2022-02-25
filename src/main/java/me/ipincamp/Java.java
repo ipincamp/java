7aef25ca3fb207cc0b9f2e01c1d4a8312e6642da
ipincamp <arif.lngsrn@gmail.com>
2022-02-25 07:39:34 +0700

    Main class

---
+++ src/main/java/me/ipincamp/Java.java
@@ -0,0 +1,22 @@
+package me.ipincamp;
+
+public class Java {
+    public static void main(String [] args) {
+        /*
+        Basic Java Course
+
+        Java language course on channel Worn Off Keys,
+        this is a basic course in 2020.
+        I made this repository as my documentation in the future.
+
+        Source: WornOffKeys: Alex zander Flores
+                W3schools  : https://www.w3schools.com/java/default.asp
+        */
+        System.out.println("ipincamp > Don't waste your time!");
+        /*
+        Hello World
+
+        System.out.println("Hello World!");
+        */
+    }
+}
