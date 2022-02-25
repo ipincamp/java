d1bf58a7780a395a9ef459729c8e01bba89d7b4d
ipincamp <arif.lngsrn@gmail.com>
2022-02-25 07:44:07 +0700

    Merged increment & decrement

---
+++ src/main/java/me/ipincamp/v4/Decrement.java
@@ -0,0 +1,17 @@
+package me.ipincamp.v4;
+
+public class Decrement {
+    public static void main(String [] args) {
+        /*
+        Decrease
+
+        name++;
+
+        ++name; for cross output
+        */
+        int myRoles = 50;
+
+        myRoles--;
+        System.out.println(myRoles);
+    }
+}
---
+++ src/main/java/me/ipincamp/v4/Increment.java
@@ -0,0 +1,13 @@
+package me.ipincamp.v4;
+
+public class Increment {
+    public static void main(String [] args) {
+        /*
+        Increase
+        */
+        int number = 50;
+
+        number++;
+        System.out.println(number);
+    }
+}
