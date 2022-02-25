9e448af70c9daed8219a43ce9eea6647315e687b
ipincamp <arif.lngsrn@gmail.com>
2022-02-25 07:43:43 +0700

    Merged arithmetic operators

---
+++ src/main/java/me/ipincamp/v3/Arithmetic.java
@@ -0,0 +1,19 @@
+package me.ipincamp.v3;
+
+public class Arithmetic {
+    public static void main(String [] args) {
+        /*
+         +	Addition	    Adds together two values (x + y)
+         -	Subtraction	    Subtracts one value from another (x - y)
+         *	Multiplication	Multiplies two values (x * y)
+         /	Division	    Divides one value by another (x / y)
+         %	Modulus	        Returns the division remainder (x % y)
+         ++	Increment	    Increases the value of a variable by 1 (++x)
+         --	Decrement	    Decreases the value of a variable by 1 (--x)
+         */
+        int number = 50;
+        number = number + 50;
+
+        System.out.println("The result is: " + number);
+    }
+}
